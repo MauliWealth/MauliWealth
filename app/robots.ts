4d9a1768ca3a8570da09f6348ca28debe82a3cdb
MauliWealth <MauliWealth2024@gmail.com>
2026-08-20 14:30:13 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const base = 'https://mauli-wealth.vercel.app'
+  const base = 'https://www.mauliwealth.com'
   return {
     rules: {
       userAgent: '*',
